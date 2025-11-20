956cf0b2ec1b585ff71cfdd712f8199e77baf375
cvirguez <cvirguezsilva@gmail.com>
2025-11-19 17:53:56 -0800

    dev: desarrollo estilos del home, pendiente mejorar los estilos de taskPage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -9,10 +9,10 @@ function HomePage(){
             <div className="home-content">
                 <div className="home-box">
                     <h1>¡Bienvenido/a, Usuario/a!</h1>
-                    <p>Organiza tus objetivos en tareas pequeñas para alcanzar tu <strong>META</strong>. Crea nuevas tareas para comenzar o consulta tus tareas si ya las creaste.</p>
+                    <p>Organiza tus objetivos en tareas pequeñas para alcanzar tus <strong>META</strong>. Crea nuevas tareas para comenzar o consulta tus tareas si ya las creaste.</p>
                     <div className="button-box">
-                        <Button variant="primary" nav="">Crear nueva Tarea</Button>
-                        <Button variant="secundary" nav="">Ver mis tareas</Button>
+                        <Button variant="primary" nav="/new-task">Crear nueva Tarea</Button>
+                        <Button variant="secundary" nav="/task">Ver mis tareas</Button>
                     </div>
                 </div>
 
